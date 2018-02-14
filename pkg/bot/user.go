2a25ef788e6d117edc699bcee80e59faa94e8a5b
Pavel <efimowpasha@yandex.ru>
2018-02-14 13:06:05 +0300

    Автоматизирован построение подкатологов

--- pkg/bot/user.go
+++ pkg/bot/user.go
@@ -2,12 +2,191 @@ package bot
 
 import (
 	"database/sql"
+	"fmt"
 
+	"../database"
+	"github.com/fatih/color"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func (tgbot *TelegramBot) AnalyzeUpdate(update tgbotapi.Update, db *sql.DB) {
 	switch {
+	case update.CallbackQuery != nil:
+		chatID := update.CallbackQuery.Message.Chat.ID
+		messageID := update.CallbackQuery.Message.MessageID
+		if database.IsUserInDatabase(chatID, db) == false {
+			color.Red(fmt.Sprintln("CallBACL: ", chatID))
+			database.AddUser(db, chatID)
+		} //ОБНУЛИТЬ ЗНАЧЕНИЯ
+
+		switch update.CallbackQuery.Data {
+		case "Одежда":
+			markup := tgbot.SendClothes(update, db)
+			edit := tgbotapi.NewEditMessageReplyMarkup(chatID, messageID, markup)
+			tgbot.Token.Send(edit)
+		case "К каталогу":
+			markup := tgbot.SendCatalog(update, db)
+			edit := tgbotapi.NewEditMessageReplyMarkup(chatID, messageID, markup)
+			tgbot.Token.Send(edit)
+		case "Женская одежда":
+			id := database.GetCatalogId(db, "Женская одежда")
+			database.SetCurrentParnetId(db, chatID, id)
+			database.SetCurrentItemByDefault(db, chatID)
+
+			markup := tgbot.SendSectionItems(update, db, id)
+			edit := tgbotapi.NewEditMessageReplyMarkup(chatID, messageID, markup)
+			tgbot.Token.Send(edit)
+		case "Мужская-женская":
+			markup := tgbot.SendClothes(update, db)
+			edit := tgbotapi.NewEditMessageReplyMarkup(chatID, messageID, markup)
+			tgbot.Token.Send(edit)
+		case "Мужская одежда":
+			id := database.GetCatalogId(db, "Мужская одежда")
+			color.Red(fmt.Sprintln(id))
+			database.SetCurrentParnetId(db, chatID, id)
+			database.SetCurrentItemByDefault(db, chatID)
+
+			markup := tgbot.SendSectionItems(update, db, id)
+			edit := tgbotapi.NewEditMessageReplyMarkup(chatID, messageID, markup)
+			tgbot.Token.Send(edit)
+		case "Каталог вперед":
+			deleteMessage := tgbotapi.DeleteMessageConfig{}
+			deleteMessage.ChatID = chatID
+			deleteMessage.MessageID = messageID
+			tgbot.Token.Send(deleteMessage)
+			current := database.GetCurrentItem(db, chatID)
+			current += 5
+			database.SetCurrentItem(db, current, chatID)
+
+			idCurrent := database.GetCurrentParnetId(db, chatID)
+			title := database.GetSectionTitle(db, idCurrent)
+			msg := tgbotapi.NewMessage(chatID, "Выберите раздел:")
+			id := database.GetCatalogId(db, title)
+			msg.ReplyMarkup = tgbot.SendSectionItems(update, db, id)
+			tgbot.Token.Send(msg)
+		case "Каталог назад":
+			deleteMessage := tgbotapi.DeleteMessageConfig{}
+			deleteMessage.ChatID = chatID
+			deleteMessage.MessageID = messageID
+			tgbot.Token.Send(deleteMessage)
+			current := database.GetCurrentItem(db, chatID)
+			current -= 5
+			database.SetCurrentItem(db, current, chatID)
+
+			idCurrent := database.GetCurrentParnetId(db, chatID)
+			title := database.GetSectionTitle(db, idCurrent)
+			msg := tgbotapi.NewMessage(chatID, "Выберите раздел:")
+			id := database.GetCatalogId(db, title)
+			msg.ReplyMarkup = tgbot.SendSectionItems(update, db, id)
+			tgbot.Token.Send(msg)
+		}
+
 	case update.Message != nil:
+		chatID := update.Message.Chat.ID
+		if database.IsUserInDatabase(chatID, db) == false {
+			color.Red(fmt.Sprintln("USUAL: ", chatID))
+			database.AddUser(db, chatID)
+		}
+
+		switch update.Message.Text {
+		case "/start":
+			tgbot.Greeting(update)
+			tgbot.SendMenu(update)
+		case "Каталог":
+			menuMsg := tgbotapi.NewMessage(chatID, "Каталог:")
+			menuMsg.ReplyMarkup = tgbot.SendMenuButton(update)
+			catalogMsg := tgbotapi.NewMessage(chatID, "Выберите раздел:")
+			catalogMsg.ReplyMarkup = tgbot.SendCatalog(update, db)
+			tgbot.Token.Send(menuMsg)
+			tgbot.Token.Send(catalogMsg)
+		case "Главное меню":
+			tgbot.SendMenu(update)
+		default:
+			tgbot.SendMenu(update)
+		}
 	}
+}
+
+func (tgbot *TelegramBot) Greeting(update tgbotapi.Update) {
+	firstName, chatID := update.Message.From.FirstName, update.Message.Chat.ID
+	msg := tgbotapi.NewMessage(chatID, fmt.Sprintf("Приветсвую Вас, %s", firstName))
+	tgbot.Token.Send(msg)
+}
+
+func (tgbot *TelegramBot) SendMenu(update tgbotapi.Update) {
+	chatID := update.Message.Chat.ID
+	msg := tgbotapi.NewMessage(chatID, "Главное меню:")
+	catalog := tgbotapi.NewKeyboardButton("Каталог")
+	bucket := tgbotapi.NewKeyboardButton("Корзина")
+	registration := tgbotapi.NewKeyboardButton("Регистрация")
+	news := tgbotapi.NewKeyboardButton("Новости")
+	keyboard := tgbotapi.ReplyKeyboardMarkup{Keyboard: [][]tgbotapi.KeyboardButton{{catalog, bucket}, {registration, news}}, ResizeKeyboard: true, OneTimeKeyboard: true}
+	msg.ReplyMarkup = keyboard
+	tgbot.Token.Send(msg)
+}
+
+func (tgbot *TelegramBot) SendCatalog(update tgbotapi.Update, db *sql.DB) tgbotapi.InlineKeyboardMarkup {
+	sections := database.GetRootSection(db)
+	keyboard := tgbotapi.InlineKeyboardMarkup{}
+	for _, section := range sections {
+		btn := tgbotapi.NewInlineKeyboardButtonData(section, section)
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{btn})
+	}
+	return keyboard
+}
+
+func (tgbot *TelegramBot) SendClothes(update tgbotapi.Update, db *sql.DB) tgbotapi.InlineKeyboardMarkup {
+	sections := database.GetClothesSection(db)
+	keyboard := tgbotapi.InlineKeyboardMarkup{}
+	for _, section := range sections {
+		btn := tgbotapi.NewInlineKeyboardButtonData(section, section)
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{btn})
+	}
+	back := tgbotapi.NewInlineKeyboardButtonData("Влдислав, назад", "К каталогу")
+	keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{back})
+	return keyboard
+}
+
+func (tgbot *TelegramBot) SendMenuButton(update tgbotapi.Update) tgbotapi.ReplyKeyboardMarkup {
+	menu := tgbotapi.NewKeyboardButton("Главное меню")
+	keyboard := tgbotapi.ReplyKeyboardMarkup{Keyboard: [][]tgbotapi.KeyboardButton{{menu}}, ResizeKeyboard: true, OneTimeKeyboard: true}
+	return keyboard
+}
+
+func (tgbot *TelegramBot) SendManClothes(update tgbotapi.Update, db *sql.DB) tgbotapi.InlineKeyboardMarkup {
+	sections := database.GetManClothes(db)
+	keyboard := tgbotapi.InlineKeyboardMarkup{}
+	for _, section := range sections {
+		btn := tgbotapi.NewInlineKeyboardButtonData(section, section)
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{btn})
+	}
+	back := tgbotapi.NewInlineKeyboardButtonData("Назад", "Мужская-женская")
+	keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{back})
+	return keyboard
+}
+
+//общая функция отправки одежды
+func (tgbot *TelegramBot) SendSectionItems(update tgbotapi.Update, db *sql.DB, id int) tgbotapi.InlineKeyboardMarkup {
+	chatID := update.CallbackQuery.Message.Chat.ID
+	current := database.GetCurrentItem(db, chatID)
+	recordsCount := database.GetRecordsCount(db, id)
+	color.Green(fmt.Sprintln("CURENT: ", current))
+	sections := database.GetClothes(db, current, id)
+	keyboard := tgbotapi.InlineKeyboardMarkup{}
+	for _, section := range sections {
+		btn := tgbotapi.NewInlineKeyboardButtonData(section, section)
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{btn})
+	}
+	back := tgbotapi.NewInlineKeyboardButtonData("🔼", "Мужская-женская")    //"Мужская-женская"
+	forward := tgbotapi.NewInlineKeyboardButtonData("➡️", "Каталог вперед") //каталог одежды назад вперед
+	torward := tgbotapi.NewInlineKeyboardButtonData("⬅️", "Каталог назад")  //каталог одежды назад вперед
+	switch {
+	case recordsCount-current <= 5:
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{torward, back})
+	case current == 0:
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{back, forward})
+	case current > 0:
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{torward, back, forward})
+	}
+	return keyboard
 }
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	c "../configuration"
+	"github.com/fatih/color"
 	"github.com/golang/glog"
 	_ "github.com/lib/pq"
 )
@@ -24,4 +25,179 @@ func OpenDB(config *c.Configuration, psqlInfo string) *sql.DB {
 		glog.Exit(err)
 	}
 	return db
+}
+
+func AddUser(db *sql.DB, id int64) {
+	stmt, err := db.Prepare(`insert into tables.users (id) values ($1)`)
+	if err != nil {
+		glog.Exit()
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		glog.Exit()
+	}
+}
+
+func IsUserInDatabase(chatID int64, db *sql.DB) bool {
+	row := db.QueryRow(`select id from tables.users where id = $1`, chatID)
+	var id string
+	row.Scan(&id)
+	color.Green(id)
+	if id == "" {
+		return false
+	}
+	return true
+}
+
+func GetRootSection(db *sql.DB) []string {
+	rows, err := db.Query(`select title from tables.catalog where id_parent = 0`)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	sections := make([]string, 0)
+	for rows.Next() {
+		section := ""
+		rows.Scan(&section)
+		sections = append(sections, section)
+	}
+	return sections
+}
+
+func GetClothesSection(db *sql.DB) []string {
+	rows, err := db.Query(`select title from tables.catalog where id_parent = 1`)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	sections := make([]string, 0)
+	for rows.Next() {
+		section := ""
+		rows.Scan(&section)
+		sections = append(sections, section)
+	}
+	return sections
+}
+
+func GetWomanClothes(db *sql.DB, current int) []string {
+	rows, err := db.Query(`select title from tables.catalog where id_parent = 3 limit 5 offset $1`, current)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	sections := make([]string, 0)
+	for rows.Next() {
+		section := ""
+		rows.Scan(&section)
+		sections = append(sections, section)
+	}
+	return sections
+}
+
+func GetClothes(db *sql.DB, current int, id int) []string {
+	rows, err := db.Query(`select title from tables.catalog where id_parent = $1 limit 5 offset $2`, id, current)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	sections := make([]string, 0)
+	for rows.Next() {
+		section := ""
+		rows.Scan(&section)
+		sections = append(sections, section)
+	}
+	return sections
+}
+
+func GetManClothes(db *sql.DB) []string {
+	rows, err := db.Query(`select title from tables.catalog where id_parent = 4`)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	sections := make([]string, 0)
+	for rows.Next() {
+		section := ""
+		rows.Scan(&section)
+		sections = append(sections, section)
+	}
+	return sections
+}
+
+func GetCurrentItem(db *sql.DB, chatID int64) int {
+	row := db.QueryRow(`select current_offset from tables.users where id = $1 `, chatID)
+	var current int
+	row.Scan(&current)
+	return current
+}
+
+func SetCurrentItem(db *sql.DB, current int, chatID int64) {
+	color.Red(fmt.Sprintln("Current: ", current))
+	stmt, err := db.Prepare(`update tables.users set current_offset = $1  where id = $2`)
+	if err != nil {
+		glog.Exit()
+	}
+	_, err = stmt.Exec(current, chatID)
+	if err != nil {
+		glog.Exit()
+	}
+}
+
+func GetCurrentParnetId(db *sql.DB, chatID int64) int {
+	row := db.QueryRow(`select id_current from tables.users where id = $1 `, chatID)
+	var current int
+	row.Scan(&current)
+	return current
+}
+
+func SetCurrentParnetId(db *sql.DB, chatID int64, id int) {
+	stmt, err := db.Prepare(`update tables.users set id_current = $1 where id = $2`)
+	if err != nil {
+		glog.Exit()
+	}
+	_, err = stmt.Exec(id, chatID)
+	if err != nil {
+		glog.Exit()
+	}
+}
+
+func SetCurrentItemByDefault(db *sql.DB, chatID int64) {
+	stmt, err := db.Prepare(`update tables.users set current_offset = 0  where id = $1`)
+	if err != nil {
+		glog.Exit()
+	}
+	_, err = stmt.Exec(chatID)
+	if err != nil {
+		glog.Exit()
+	}
+}
+
+//select count(id_parent) from tables.catalog where id_parent = 3;
+/*func GetRecordsCount(db *sql.DB) int { //передаем айди
+	row := db.QueryRow(`select count(id_parent) from tables.catalog where id_parent = 3`)
+	var count int
+	row.Scan(&count)
+	return count
+}*/
+
+func GetRecordsCount(db *sql.DB, id int) int { //передаем айди
+	row := db.QueryRow(`select count(id_parent) from tables.catalog where id_parent = $1`, id)
+	var count int
+	row.Scan(&count)
+	return count
+}
+
+func GetCatalogId(db *sql.DB, title string) int { //передаем айди
+	row := db.QueryRow(`select id from tables.catalog where title = $1`, title)
+	var id int
+	row.Scan(&id)
+	return id
+}
+
+func GetSectionTitle(db *sql.DB, id int) string {
+	row := db.QueryRow(`select title from tables.catalog where id = $1 `, id)
+	var title string
+	row.Scan(&title)
+	return title
 }
