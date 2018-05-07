999e4f1b83f7d39f788a0f14e2e81888b8746271
PavelEfimov <efimowpasha@yandex.ru>
2018-05-07 11:52:32 +0300

    Дополнили базу

---
+++ Мужская одежда.sql
@@ -0,0 +1,127 @@
+﻿/*Мужская одежда*/
+
+/*куртки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Куртка с капюшоном', 3999,'темно-синий', 36, 'Без подкладки, технологичные детали, карман на рукаве, ветрозащитная планка состав: 92% полиэстер, 8% эластан', 'AgADAgADvKgxG0R7gEvVajKtUX72WrwJnA4ABFYpEmycrS6_JkwEAAEC'),
+('Куртка из перфорированной ткани', 3499,'темно-синий', 36, 'Воротник гольф, трикотажная резинка на манжетах, карман на рукаве, состав: 100% полиэстер', 'AgADAgADvagxG0R7gEt5md1aGC4_ZaokrQ4ABO1MFIYLaEE0Sl8BAAEC'),
+('Мужская куртка', 2999,'чёрно-синий', 36, 'Натуральная ткань, воротник-стойка, ветрозащитная планка, 4 кармана, состав: 100% хлопок', 'AgADAgADvqgxG0R7gEtOEtcVStx_qkikmg4ABANlc3GPaRqvyEAEAAEC'),
+('Стеганная куртка', 1499,'светло-серый', 36, 'Ультралегкая, утепленная, ветрозащитная планка, 2 кармана, состав: 100% полиэстер', 'AgADAgADv6gxG0R7gEuxG6BAk5O_xICgmg4ABGsIYfzFiXGSvUkEAAEC'),
+('Ультралегкая куртка с капюшоном', 1999,'сине-зелёный', 36, 'Ультралегкая, несъемный капюшон, ветрозащитная планка, 2 внешних кармана, состав: 100% нейлон', 'AgADAgAD6KgxGwkUgUulLq1tFFsb7fdUqw4ABNmmcNHvGQ7bR18BAAEC'),
+('Куртка-бомбер', 3999,'индиго', 36, 'Ткань с водоотталкивающей пропиткой, воротник гольф, ветрозащитная планка, 5 карманов, состав: 100% нейлон', 'AgADAgADwKgxG0R7gEuPt9x8m6TF3R0OnA4ABByoATFOiI3DbUQEAAEC');
+
+/*пальто*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Пальто', 10625,'черный', 37, 'Пальто мужское силуэта Slim-Fit, застежка двубортная, воротник отложной на стойке, состав: 50% шерсть, 50% полиамид', 'AgADAgAD66gxGwkUgUtXN-J2jigLhobMRg4ABFM0VvbJ6Ie1y-wBAAEC'),
+('Укороченное пальто', 5999,'темно-синий', 37, 'Из плотного полушерстяного текстиля, модель прямого кроя, состав: 65% шерсть, 35% полиэстер', 'AgADAgAD7KgxGwkUgUv_EXNy8Y4B2GIInA4ABHhxEngxsfhOokYEAAEC'),
+('Полупальто', 9990,'', 37, 'Из плотного текстиля с шерстью, воротник с лацканами, двубортная застежка, состав: 75% шерсть, 25% полиэстер', 'AgADAgAD7agxGwkUgUuhpNob3PxscdHArA4ABJkOXQABfwKRtsVeAQABAg'),
+('Пальто', 14999,'темно-синий', 37, 'Из шерстяного текстиля, модель прямого силуэта с тонким слоем утеплителя, состав: 70% шерсть, 30% полиамид', 'AgADAgAD7qgxGwkUgUvh9FhlbDLEMDiaQw4ABLbYk4oPIXG65kQEAAEC'),
+('Пальто', 6990,'', 37, 'Из плотного смесового текстиля с добавлением шерсти, модель приталенного кроя, состав: 20% шерсть, 80% полиэстер', 'AgADAgAD76gxGwkUgUsfN3kAAY4v_FW-DJwOAARYgQa93AwmsxJIBAABAg'),
+('Полупальто', 10990,'', 37, 'И мягкого драпа с высоким содержанием шерсти, модель приталенного кроя, состав: 70% шерсть, 20% полиэстер', 'AgADAgAD8KgxGwkUgUtZxD1m-8PZDqRWqw4ABA-YnT5ywjZ1NWEBAAEC');
+
+
+/*бомберы*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Куртка-бомбер', 3499,'черный', 38, 'Ткань с водоотталкивающей пропиткой, подкладка из сетки, ветрозащитная планка, состав: 100% нейлон ', 'AgADAgADqqgxG0R7gEuokKraFA1_Z7EgrQ4ABIhrY8zA0Bow3V4BAAEC'),
+('Бомбер с воротником-стойкой', 3999,'чёрный', 38, 'Подкладка из сетки, воротник-стойка, ветрозащитная планка, 5 карманов, состав: 100% полиэстер', 'AgADAgADq6gxG0R7gEtVI-xK95Xpd5thqw4ABCl4gUmycrofm2MBAAEC'),
+('Куртка-бомбер', 2999,'черно-синий', 38, 'Ткань с водоотталкивающей пропиткой, воротник гольф, ветрозащитная планка, 4 кармана, состав: 100% полиэстер', 'AgADAgAD2qgxGwkUgUuIT4VrplVyyxgEnA4ABCv-n81mSXYBXkgEAAEC'),
+('Бомбер с капюшоном', 2999,'черный', 38, 'Ткань с водоотталкивающей пропиткой, подкладка из сетки, ветрозащитная планка, 4 кармана, состав: 100% полиэстер', 'AgADAgADrKgxG0R7gEscYIxDAWWFzc4RnA4ABG9Mp730CJPVeUgEAAEC'),
+('Кожаный бомбер', 2999,'глубокий синий', 38, 'Бомбер из экокожи, воротник гольф, ветрозащитная планка, 4 кармана, состав: 100% полиэстер', 'AgADAgADragxG0R7gEttPej7nNWBroLdrA4ABPKex_l_AYLbpWEBAAEC'),
+('Кожаный бомбер', 2999,'черный', 38, 'Бомбер из перфорированной экокожи, несъёмный капюшон, ветрозащитная планка, 4 кармана, состав: 100% полиэстер', 'AgADAgADrqgxG0R7gEtLi1iR1Rrgiii0qw4ABGQtefsKcTGdQGEBAAEC');
+
+
+/*парки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Парка с капюшоном', 3499,'черно-синий', 39, 'Ткань с водоотталкивающей пропиткой, кулиска по линии талии, ветрозащитная планка, 5 карманов, состав: 100% полиэстер', 'AgADAgADw6gxG0R7gEs73aMjuWC_ryDJRg4ABJlAX5VHaJkEBegBAAEC'),
+('Парка в камуфляжный принт', 2999,'темно-оливковый', 39, 'Натуральная ткань, несъёмный капюшон, ветрозащитная планка, 4 кармана, состав: 100% хлопок', 'AgADAgADxagxG0R7gEuBpivb3nQKRQehmg4ABFkMIYUCoy_Hpj4EAAEC'),
+('Парка с капюшоном', 2499,'темно-зеленый', 39, 'Натуральная ткань, несъёмный капюшон, кулиска по линии талии, ветрозащитная планка, 7 карманов, состав: 100% хлопок', 'AgADAgAD8agxGwkUgUvhhlFoUJseZ1LBrA4ABCoBUhgAAee5Df9hAQABAg'),
+('Парка с капюшоном', 2499,'темно-синий', 39, 'Натуральная ткань, центральная застёжка на молнию, кулиска по линии талии, ветрозащитная планка, 7 карманов, состав: 100% хлопок', 'AgADAgADxqgxG0R7gEv6T8-DWmdsW7Gjmg4ABEuCaOOLE83x_D0EAAEC');
+
+
+
+/*Джемперы*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Двухслойный джемпер', 1399,'графитовый', 40, 'Из хлопковой ткани, круглый вырез горловины, длинные двухслойные рукава, текстовый принт, состав: 100% хлопок', 'AgADAgAD3qgxGwkUgUu9R_qdFLiCu5EFnA4ABEt2M00JiaTArkMEAAEC'),
+('Двухслойный джемпер', 999,'светло-серый', 40, 'Из фактурного трикотажа, круглый вырез горловины, длинные двухслойные рукава, состав: 60% хлопок, 40% полиэстер', 'AgADAgADtagxG0R7gEseYGzoBkOziGsYrQ4ABHZT7E4mkCwr3mIBAAEC'),
+('Джемпер с карманом из нейлона', 1799,'жемчужно-белый', 40, 'Из хлопковой ткани, круглый вырез горловины, карман кенгуру, состав: 62% хлопок, 37% полиэстер', 'AgADAgADtqgxG0R7gEtW0DIXsJhjl3zLRg4ABPzCQVVTBIr61eUBAAEC'),
+('Джемпер с принтом', 1599,'индиго', 40, 'Из мягкой ткани, круглый вырез горловины, длинные рукава, градиентный текстовый принт, состав: 60% хлопок, 40% полиэстер', 'AgADAgAD36gxGwkUgUvdbCsUCtzZ1g8anA4ABOdUdr87mLil70IEAAEC'),
+('Джемпер в полоску', 1599,'чёрно-синий', 40, 'Из хлопка, круглый вырез горловины, длинные рукава, состав: 100% хлопок', 'AgADAgAD4KgxGwkUgUvCGOnVhGQlJJBOqw4ABGfYLq7H3cP_Z14BAAEC'),
+('Комбинированный джемпер', 1199,'красный', 40, 'Из мягкого трикотажа, круглый вырез горловины, длинные рукава, текстовый принт, состав: 60% хлопок, 40% полиэстер', 'AgADAgAD4agxGwkUgUvSbtiZISkYa5Sjmg4ABFiS8eM0F3TGMk0EAAEC'),
+('Джемпер с принтом цифры', 799,'черно-синий', 40, 'Из мягкого трикотажа, круглый вырез горловины, длинные рукава, принт цифры, состав: 35% хлопок, 65% полиэстер', 'AgADAgADt6gxG0R7gEvEmuGIkzjG9icWnA4ABJT5lm--cGrhqUcEAAEC'),
+('Джемпер с колорблокингом', 1599,'белый', 40, 'Из хлопка, круглый вырез горловины, длинные рукава, состав: 100% хлопок', 'AgADAgADuKgxG0R7gEuNxEw4nBMdl9Niqw4ABCK-DmJj1nzggl8BAAEC');
+
+
+
+/*свитеры*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Свитер', 1599,'Синий', 41, 'Хорошее качество, теплый и элегантный свитер, состав: 50% акрил, 50% хлопок', 'AgADAgADzqgxG0R7gEvGAAEPeMzh5pQ6H60OAAStLvx4HRvF2ChgAQABAg'),
+('Свитер', 2999,'светло-серый', 41, 'Удобный свитер, теплый и элегантный, состав: 100% хлопок', 'AgADAgADz6gxG0R7gEsVPmFlAhkl_TPHRg4ABJshOGtl-9CQz-oBAAEC');
+
+/*брюки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Брюки 5 карманов', 1799,'светло-серый', 42, 'Из тонкого хлопка, прямой крой, застёжка на пуговицу и молнию, состав: 100% хлопок', 'AgADAgADr6gxG0R7gEvPJ0oZyHhILyc2qw4ABDdeZZQN-D3z0WkBAAEC'),
+('Зауженные брюки из хлопка', 1199,'темно-серый', 42, 'Из лёгкой хлопковой ткани, пояс со шнурком, 4 кармана, состав: 100% хлопок', 'AgADAgADsKgxG0R7gEtwkXywmKrzjQfdrA4ABC7GFIliCKaHul4BAAEC'),
+('Прямые брюки из льна', 2499,'темно-серый', 42, 'Зауженный крой, эластичный пояс с утяжкой, 4 кармана, состав: 100% лён', 'AgADAgAD3KgxGwkUgUun8H6I6i_HmZC2mg4ABIk7YsEwhR8sYkIEAAEC'),
+('Брюки чиносы в мелкую клетку', 3499,'ярко-синий', 42, 'Из смеси хлопка, льна, модала и эластана, свободный крой, 2 кармана, состав: 37% хлопок, 35% лиоцелл, 15% лён', 'AgADAgAD3agxGwkUgUvDsSIk3CovjaK1mg4ABDNE6InlkG4OakUEAAEC'),
+('Брюки с декоративным поясом', 2799,'серо-бежевый', 42, 'Из хлопковой ткани, прилегающий крой, застёжка на пуговицу и молнию, состав: 100% хлопок', 'AgADAgADsagxG0R7gEs5zQpLEkVLdaA3qw4ABBTf0onxIROcsW0BAAEC'),
+('Брюки-джоггеры из тонкого твила', 1799,'графитовый', 42, 'Зауженный крой, эластичный пояс с утяжкой, 4 кармана, состав: 100% хлопок', 'AgADAgADsqgxG0R7gEu2X0sZH5J1F84DnA4ABCeP_Ap-Z0LvfUcEAAEC'), 
+('Брюки чиносы из хлопка', 2799,'глубокий синий', 42, 'Из структурной хлопковой ткани, свободный крой, 2 кармана состав: 70% хлопок, 28% модал, 2% эластан', 'AgADAgADs6gxG0R7gEvzsBsajhoIKAABwKwOAATasO8ZV32f2gdfAQABAg'),
+('Брюки 5 карманов из легкого хлопка', 1599,'тёмно-синий', 42, 'Из легкой хлопковой ткани, застёжка на пуговицу и молнию, 5 карманов, состав: 100% хлопок', 'AgADAgADtKgxG0R7gEvb2o-gZoQIWqebmg4ABKtmT55f9XBNwUgEAAEC'),;
+
+/*джинсы*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Базовые прямые джинсы', 1799,'светло-синий', 43, 'Прямой силуэт, низкая посадка по талии, 5 карманов, состав: 100% хлопок', 'AgADAgADuagxG0R7gEv1L9R5osS-gO7MrA4ABAsq0UetARo8imEBAAEC'),
+('Джинсы с дырками на коленях', 1799,'голубой', 43, 'Узкий силуэт, заниженная посадка по талии, комфортная эластичная ткань, состав: 100% хлопок', 'AgADAgAD4qgxGwkUgUuLF-lOsw6bftzLrA4ABE0B_UzMq_R0p2ABAAEC'),
+('Тёмные джоггеры', 1199,'синий', 43, 'Средняя посадка по талии, облегченная ткань, умеренная стирка, состав: 100% хлопок', 'AgADAgAD46gxGwkUgUvn1Y5PlbJsyza5mg4ABKxZvTLL7kaKOUUEAAEC'),
+('Базовые зауженные джинсы', 1799,'светло-синий', 43, 'Зауженный силуэт, низкая посадка по талии, 5 карманов, состав: 100% хлопок', 'AgADAgAD5KgxGwkUgUvjZIj3rKtVkZqzqw4ABH1JYOiBJaP_414BAAEC'),
+('Зауженные джинсы', 2499,'светло-синий', 43, 'Узкий силуэт, заниженный уровень талии, комфортная эластичная ткань, состав: 100% хлопок', 'AgADAgAD5agxGwkUgUu5zGDZb5fJT14rrQ4ABDuGiPMoZ_p40F8BAAEC'),
+('Свободные джинсы', 1999,'голубой', 43, 'Легкие потертости, свободный силуэт, средний уровень талии, плотный деним, состав: 100% хлопок', 'AgADAgADuqgxG0R7gEs7YbngorYJLikZnA4ABJgF693NeFyI70gEAAEC');
+
+/*рубашки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Рубашка с этническим принтом', 1399,'белый', 32, 'Классический воротник, короткие рукава, застёжка на пуговицах, состав: 100% хлопок', 'AgADAgAD9agxGwkUgUtJvLvTXxGCcmakmg4ABGXUC9dNWYUwmkUEAAEC'),
+('Рубашка из хлопка и льна', 1799,'ментоловый', 32, 'Классический воротник, короткие рукава, застёжка на пуговицах, состав: 57% хлопок, 43% льна', 'AgADAgADyagxG0R7gEuCFuUbWgwRpDydmg4ABNnTOUIEYCVOPUkEAAEC'),
+('Рубашка с денимной стиркой', 1599,'голубой', 32, 'Классический воротник, короткие рукава, застёжка на пуговицах, состав: 100% лиоцел', 'AgADAgAD9qgxGwkUgUtTMUYVHsKtXtpOqw4ABAfGoZ8QWROAYGABAAEC'),
+('Рубашка из хлопка с морским принтом', 1399,'белый', 32, 'Воротник-стойка, короткие рукава, полуприлегающий силуэт, состав: 100% хлопок', 'AgADAgADyqgxG0R7gEsZZBwL2l6Px6Vjqw4ABIoCaEZVMQJR9mABAAEC'),
+('Рубашка с короткими рукавами', 1599,'голубой', 32, 'Классический воротник, короткие рукава, пуговицы, состав: 100% хлопок', 'AgADAgADy6gxG0R7gEvSt5Ogm8ymuPzIRg4ABDVAUP_4Sf0c0u4BAAEC'),
+('Рубашка из льна с хлопком', 2499,'голубой', 32, 'Из смеси хлопка и льна, длинные рукава, полуприлегающий силуэт, состав: 55% лён, 45% хлопок', 'AgADAgADzKgxG0R7gEvhlTkAAc3fx6kzzKwOAARY2ijNmEwuktRhAQABAg'),
+('Рубашка из хлопка с рок-принтом', 1399,'черный', 32, 'Классический воротник, длинные рукава, манжеты на пуговицах, состав: 100% хлопок', 'AgADAgADzagxG0R7gEsFf98OkATPgYCkmg4ABD2llf9jyflv6UwEAAEC');
+
+/*футболки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Футболка с тропическим принтом', 899,'белый', 44, 'Круглый вырез горловины, короткие рукава, принт листья, состав: 100% хлопок', 'AgADAgAD0qgxG0R7gEuVG8thtRf2fEEjrQ4ABGHIJe-mj75_w14BAAEC'),
+('Футболка в мелкий принт', 899,'графитовый', 44, 'Круглый вырез горловины, короткие рукава, мелкий принт носороги, состав: 100% хлопок', 'AgADAgAD06gxG0R7gEsx-9Teu0dtHCRfqw4ABPZYWyzApS87ZGMBAAEC'),
+('Футболка с текстовым принтом', 699,'ярко-коралловый', 44, 'Круглый вырез горловины, короткие рукава, текстовый принт, состав: 100% хлопок', 'AgADAgAD_qgxGwkUgUuviaGe-jnCoc2bmg4ABG2HeAP6zDVu4EQEAAEC'),
+('Футболка с тропическим принтом', 799,'белый', 44, 'Круглый вырез горловины, короткие рукава, тропический принт, состав: 100% хлопок', 'AgADAgAD1KgxG0R7gEvsdKzEbK_SMXDzrA4ABHXaiPcAAcESxmNeAQABAg'),
+('Футболка с принтом', 999,'черно-синий', 44, 'Круглый вырез горловины, короткие рукава, текстовый принт, состав: 100% хлопок', 'AgADAgAD1agxG0R7gEtoHdNPJS9cXTfERg4ABN90eAmTmkGpQekBAAEC'),
+('Поло в полоску', 1199,'белый', 44, 'Воротник на пуговицах, короткие рукава, горизонтальная полоска, состав: 100% хлопок', 'AgADAgAD_6gxGwkUgUtRmaM1_vTjJGNPqw4ABPIlaumosFbzkmABAAEC'),
+('Футболка с фотопринтом', 999,'белый', 44, 'из хлопковой ткани, круглый вырез горловины, короткие рукава, фотопринт спереди, состав: 100% хлопок', 'AgADAgAD1qgxG0R7gEuzcKAhKQO9LeEorQ4ABIha7hn8gbyV3WEBAAEC');
+
+/*майки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Майка с тропическим принтом', 699,'темно-бирюзовый', 45, 'Круглый вырез горловины, без рукавов, принт листья, состав: 100% хлопок', 'AgADAgAD6agxGwkUgUvfvuuLa-o-5UDFRg4ABEDId9dzZJ590OsBAAEC'),
+('Майка с тропическим принтом', 699,'серый', 45, 'Круглый вырез горловины, без рукавов, принт листья, состав: 100% хлопок', 'AgADAgAD6qgxGwkUgUs53kLVnLzu1PP0rA4ABOmxNNNSMP0IuV8BAAEC'),
+('Базовая майка из риба', 499,'черный', 45, 'Из однотонной хлопковой ткани, круглый вырез горловины, без рукавов, состав: 98% хлопок, 2% эластан', 'AgADAgADwagxG0R7gEsxNpU-Nz982QvHRg4ABFYm9Ol0mR8b5ewBAAEC'),
+('Базовая майка из риба', 499,'белый', 45, 'Из однотонной хлопковой ткани, без рукавов, состав: 98% хлопок, 2% эластан', 'AgADAgADwqgxG0R7gEtT9LBuAWOAu8ImrQ4ABLPS1VWkYHk0il8BAAEC');
+
+/*пиджаки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Пиджак из ткани со льном', 5999,'ярко-синий', 46, 'Полуприлегающий пиджак в мелкую клетку, маленький карман для мелочей, нагрудной карман, состав: 37% хлопок, 35% лиоцелл, 15% лён', 'AgADAgAD8qgxGwkUgUvU4gceeummIG-amg4ABKokLEduAw5JHUgEAAEC'),
+('Трикотажный пиджак', 4999,'черно-синий', 46, 'Пиджак с геометрическим узором, нагрудной карман, внутренний карман, состав: 60% хлопок, 40% полиэстер', 'AgADAgAD86gxGwkUgUvxLVMMLpEKK4LxrA4ABKSqM7499YPBQ2EBAAEC'),
+('Трикотажный пиджак', 4999,'черно-синий', 46, 'Из эластичного полотна, воротник с прямоугольными лацканами, два боковых кармана, состав: 81% полиэстер, 19% вискоза', 'AgADAgADx6gxG0R7gEtHXEyrV0Q7SDbcrA4ABHGCJg8S1SA_NmEBAAEC'),
+('Пиджак из структурного хлопка', 5999,'ярко-голубой', 46, 'Полуприлигающий пиджак, два боковых прорезных кармана, нагрудный карман, состав: 35% хлопок, 65% полиэстер', 'AgADAgAD9KgxGwkUgUtJNPGMMRRCrtQKnA4ABJiv5YMtVutQLT0EAAEC'),
+('Пиджак в клетку из хлопка', 5999,'глубокий синий', 46, 'Полуприталенный крой, два боковых кармана, воротник с прямоугольными лацканами состав: 60% хлопок, 40% полиэстер', 'AgADAgADyKgxG0R7gEtilqMwz0adyeWYQw4ABE4FyOdJ5D7EykMEAAEC');
+
+/*жакеты*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Жакет из хлопка', 2999,'черно-синий', 47, 'С капюшоном, длинные рукава, застёжка на молнию, без подкладки, состав: 100% хлопок', 'AgADAgADu6gxG0R7gEuBlG6mfUlOPeHcrA4ABBodB8XdSHkSJWMBAAEC'),
+('Джинсовый жакет', 2999,'пыльно-голубой', 47, 'Классический воротник, длинные рукава, застежка на болтах, состав: 100% хлопок', 'AgADAgAD5qgxGwkUgUstRqp7HXJ7s8wFnA4ABPPvl7juvuGZiUEEAAEC'),
+('Жакет на молнии', 2999,'черно-синий', 47, 'Жакет на молнии, воротник стойка, боковые прорезные карманы, состав: 98% полиэстер, 2% эластан', 'AgADAgAD56gxGwkUgUtohRTjw1VMoQvKRg4ABMW6kjr3IdK-K-0BAAEC');
+
+/*толстовки*/
+INSERT INTO tables.products (title, price, color, id_category, description, photo) VALUES 
+('Толстовка с вышивкой череп', 1799,'черный', 35, 'Круглый вырез горловины, длинные рукава, крупная вышивка череп, состав: 100% хлопок', 'AgADAgAD-qgxGwkUgUs0njiN-tVg9tGxqw4ABFoOAmBbLvYiul8BAAEC'),
+('Толстовка с принтом', 1799,'белый', 35, 'Круглый вырез горловины, длинные рукава, принт розы, состав: 99% хлопок, 1% вискоза', 'AgADAgAD-6gxGwkUgUt92V7SPOs6HpbBrA4ABHhR6WDH-jqLqF0BAAEC'),
+('Толстовка на молнии', 2999,'серый', 35, 'Из нейлона, капюшон, контрастная отделка, карманы на молниях, эластичные манжеты, состав: 80% хлопок, 20% полиэстер', 'AgADAgAD_KgxGwkUgUtfMfKkoLguN3NTqw4ABGI5K3TYp2pgK10BAAEC'),
+('Яркая толстовка с принтом', 1599,'оранжевый', 35, 'Из мягкого трикотажа, капюшон, текстовый принт, длинные рукава, эластичные манжеты, состав: 60% хлопок, 40% полиэстер', 'AgADAgAD0KgxG0R7gEss2mll0YNKkAfDRg4ABDXQltrtLEKd6esBAAEC'),
+('Камуфляжная толстовка', 1999,'тёмный хаки', 35, 'Капюшон, камуфляжный принт, длинные рукава, эластичные манжеты, состав: 100% полиэстер', 'AgADAgAD0agxG0R7gEtuQqspgLyVlOAarQ4ABAx8GsvbpT-BqlsBAAEC');
+
